1c8541bc730e9a1be07f3336aae193be58f9ccc3
Integralist <mark.mcdx@gmail.com>
2014-02-16 19:51:00 +0000

    Pending tests

--- spec/cinderella_spec.rb
+++ spec/cinderella_spec.rb
@@ -1,7 +1,26 @@
 require 'spec_helper'
 
 describe Cinderella do
-  it 'does stuff' do
-    pending
+  let(:data) {{ :key => 'value' }}
+  let(:till_midnight) { 60 * 60 }
+
+  describe '.transforms(data, till_midnight)' do
+    it 'returns a hash of the passed data' do
+      pending
+    end
+
+    it 'stores original and transformed data' do
+      pending
+    end
+
+    it 'restores the data to its original state after set time' do
+      pending
+    end
+  end
+
+  describe '.midnight(id)' do
+    it 'restores the data to its original state' do
+      pending
+    end
   end
 end
