d43f9ab2c5dac4a066f7c4a6663936be4651f927
Integralist <mark.mcdx@gmail.com>
2014-02-17 16:59:08 +0000

    First DataStore spec implemented and passing

--- spec/data_store_spec.rb
+++ spec/data_store_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
 describe DataStore do
+  let(:instance)    { DataStore.instance }
+  let(:original)    { 'bar' }
+  let(:transformed) { 'BAR' }
+
   describe 'set(data)' do
     it 'stores original and transformed data' do
-      pending
+      instance.set({
+        :id => 'foo',
+        :original => original,
+        :transformed => transformed
+      })
+
+      expect(instance.get('foo')[:original]).to eq(original)
+      expect(instance.get('foo')[:transformed]).to eq(transformed)
     end
   end
 
