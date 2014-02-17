78fb862b2e784eff18736337ec6874b4bdccca6b
Integralist <mark.mcdx@gmail.com>
2014-02-17 14:44:15 +0000

    Create a new spec file for the DataStore

---
+++ spec/data_store_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe DataStore do
+  describe 'set(data)' do
+    it 'stores original and transformed data' do
+      pending
+    end
+  end
+
+  describe 'get(id)' do
+    it 'returns data hash' do
+      pending
+    end
+  end
+end
